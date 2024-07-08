148d42dc425f339fd9e41651342823ca27a90598
Eliud-Marierie-Biketi <elmariebiks03@gmail.com>
2024-07-08 08:59:15 +0300

    App.py local host routing- for phone experience

--- server/app.py
+++ server/app.py
@@ -622,5 +622,5 @@ api.add_resource(PeriodicTreatmentDeleteResource, '/periodic_treatment/<int:trea
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
 
